b5a33d1c325cb1c414eb3c5418c13bc6edb233aa
kavyamnaier123 <kavyamnair7@gmail.com>
2024-06-30 12:40:27 +0530

    create smartapp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import AddSmart from './components/AddSmart';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SearchSmart from './components/SearchSmart';
+import DeleteSmart from './components/DeleteSmart';
+import ViewAll from './components/ViewAll';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
    <Routes>
     <Route path='/'  element={<AddSmart/>}/>
     <Route path='/search'  element={<SearchSmart/>}/>
+    <Route path='/delete'  element={<DeleteSmart/>}/>
+    <Route path='/viewall'  element={<ViewAll/>}/>
    </Routes>
    </BrowserRouter>
   );
